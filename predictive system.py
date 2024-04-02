52b6811191d488dd6c9ba5209b983f291b511862
Shrutakeerti_1111 <shrutakeerti1601@gmail.com>
2024-04-03 01:11:52 +0530

    Create predictive system.py

---
+++ predictive system.py
@@ -0,0 +1,22 @@
+import numpy as np
+import pickle 
+
+
+loaded_model=pickle.load(open('C:/Users/Shrutakeerti Datta/OneDrive/Desktop/Machine Deployment/trained_model.sav','rb'))
+
+
+input_data = (5,166,72,19,175,25.8,0.587,51)
+
+#changing the input_data to numpy array
+input_data_as_numpy_array = np.asarray(input_data)
+
+#reshape the array as we are the predicting for one instance
+input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+
+prediction =  loaded_model.predict(input_data_reshaped)
+print(prediction)
+
+if(prediction[0] == 0):
+  print('the person is not diabetic')
+else:
+  print('the person is diabetic')
